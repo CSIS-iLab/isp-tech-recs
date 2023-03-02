8a9270583f8274e5e72b11abf7f0cb4d72aa7251
jdromero88 <jdromerov88@gmail.com>
2023-03-02 16:13:17 -0500

    Breaking the about section into smallers components

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,5 +1,7 @@
 <script>
   import Icon from './Icons.svelte'
+  import DevelopmentTeam from './DevelopmentTeam.svelte'
+  import AssociatedReports from './AssociatedReports.svelte'
 
   export let reportsDataset
 </script>
@@ -18,54 +20,11 @@
         This website is made possible by support from Amazon Web Services.
       </p>
     </div>
-    <div class="about__reports">
-      <p class="about__reports__title">Associated Reports</p>
-      <ul>
-        {#each reportsDataset.reports as report}
-        <li>
-          <a
-            class="about__reports__link"
-            href="{report.link}"
-            target="_blank"
-            rel="noreferrer"
-            >{report.title}<span
-              class="icon-container"
-              ><Icon class="icon__report" name="Icon-open-blank" /></span
-            ></a
-          >
-        {/each}
 
-      </ul>
-    </div>
-    <!-- <div class="about__initiative">
-      <p>
-        The <a
-          class="about__initiative__link"
-          href="https://www.csis.org/programs/energy-security-and-climate-change-program/projects/clean-resilient-states-initiative"
-          target="_blank"
-          rel="noreferrer">Technology Policy Recommendations Initiative</a
-        > is a project developed by the CSIS International Security Program
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-      </p>
-    </div> -->
-    <div class="about__development">
-      <h3 class="about__development__title">Development Team</h3>
-      <p class="about__development__description">
-        The Technology Policy Recommendations database is a product of the <a
-          class="about__development__link"
-          href="https://www.csis.org/programs/dracopoulos-ideas-lab"
-          target="_blank"
-          rel="noreferrer">Andreas C. Dracopoulos iDeas Lab</a
-        >, the in-house digital, multimedia, and design agency at the Center for
-        Strategic and International Studies.
-      </p>
-      <p class="about__development__description">
-        Produced by Christina Hamm, Tucker Harris, Serven Maraghi, José Romero.
-      </p>
-    </div>
+    <AssociatedReports {reportsDataset} />
+
+    <DevelopmentTeam />
+
     <div class="back-top-container">
       <a class="back-top-container__link" href="#top"
         ><span>Back to Top</span><span
---
+++ src/components/AssociatedReports.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Icon from "./Icons.svelte"
+
+  export let reportsDataset
+</script>
+
+<div class="reports">
+  <p class="reports__title">Associated Reports</p>
+  <ul>
+    {#each reportsDataset.reports as report}
+      <li>
+        <a
+          class="reports__link"
+          href={report.link}
+          target="_blank"
+          rel="noreferrer"
+          >{report.title}<span class="icon-container"
+            ><Icon class="icon__report" name="Icon-open-blank" /></span
+          ></a
+        >
+      </li>{/each}
+  </ul>
+</div>
+<style lang="scss">
+  @use '../scss/components/associated-reports';
+</style>
